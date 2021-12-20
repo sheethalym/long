89d5e4def32ebf7f98c54d15ca10ddbf5f27930a
sheethalym <sheethal.xworkz@gmail.com>
2021-12-20 21:41:16 +0530

    adding long program

---
+++ long.java
@@ -0,0 +1,17 @@
+class Long{
+ public static void main(String[]args){
+  long []phno={9876543210l,9765432180l,9765432180l,9654321870l,9543218760l,9432145600l,9321456789l,8765432109l,8123456789l,8098765432l};
+  System.out.println(phno[0]);
+  System.out.println(phno[1]);
+  System.out.println(phno[2]);
+  System.out.println(phno[3]);
+  System.out.println(phno[4]);
+  System.out.println(phno[5]);
+  System.out.println(phno[6]);
+  System.out.println(phno[7]);
+  System.out.println(phno[8]);
+  System.out.println(phno[9]);
+
+}
+
+}
